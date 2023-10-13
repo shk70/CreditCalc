66cd3e68d7e289c4e0dbad5df4a76983aaa7221f
shk70 <shk70@mail.ru>
2023-10-13 17:47:29 +0300

    Улучшил немного интерфейс

--- app/src/main/java/ru/ultrashk/creditcalc/model/CalcViewModel.kt
+++ app/src/main/java/ru/ultrashk/creditcalc/model/CalcViewModel.kt
@@ -3,6 +3,7 @@ package ru.ultrashk.creditcalc.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.Locale
 
 //import androidx.lifecycle.LiveData
 //import androidx.lifecycle.MutableLiveData
@@ -36,7 +37,9 @@ class CalcViewModel: ViewModel() {
     fun getPaymentPerMonth()  {
         var topPart = _sum.value!! * monthRate
         var bottomPart: Double = (1 - (1/Math.pow(monthRate+1, _month.value!!.toDouble()))).toDouble()
-        _monthPay.value = String.format("%.02f",topPart/bottomPart).toDouble()
+        _monthPay.value = String.format(Locale.ROOT, "%.02f",topPart/bottomPart).toDouble()
+        //_monthPay.value = topPart/bottomPart
+
     }
 
     fun getGrafik() {
@@ -48,9 +51,9 @@ class CalcViewModel: ViewModel() {
             var graf = Grafik()
             graf.month = i
             graf.payPerMonth = mm
-            graf.sumPercent = String.format("%.02f", dolg!!*monthRate).toDouble()
-            graf.sumDolg = String.format("%.02f", mm-dolg!!*monthRate).toDouble()
-            dolg = String.format("%.02f", dolg!! - (mm-dolg!!*monthRate)).toDouble()
+            graf.sumPercent = String.format(Locale.ROOT,"%.02f", dolg!!*monthRate).toDouble()
+            graf.sumDolg = String.format(Locale.ROOT,"%.02f", mm-dolg!!*monthRate).toDouble()
+            dolg = String.format(Locale.ROOT,"%.02f", dolg!! - (mm-dolg!!*monthRate)).toDouble()
             if(dolg<0.00) dolg=0.00
             graf.ostatok = dolg;
             arr.add(graf)
